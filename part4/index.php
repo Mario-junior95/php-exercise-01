0fa5ecc71deac3aaa7af50c6abf134d41e570f68
Mario-junior95 <karehmario@gmail.com>
2020-10-12 10:59:51 +0300

    Completed Part 4

--- part4/index.php
+++ part4/index.php
@@ -3,44 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Part 3</title>
+    <title>Part 4</title>
 </head>
 <body>
-<?php
- function palindrome ($str){
-   if(is_string($str) && $str !== ""){
-     $str1 = strtolower($str);
-     $str2 = strrev($str1);
-      if($str2 === $str1){
-        echo "true" ;
-     }else{
-       echo "false";
-     }
-   }
-   else{
-      $alert = <<<ALERT
-      <script>
-      alert("You should insert a string not a number not a empty String");
-      </script>
-      ALERT;
-      echo $alert;
-   }
-   return $str;
- }
+    <h1>Multidimensional Arrays</h1>
+    <?php
+      $arr = array("musicals" => array( 0 => "Oklahoma" , 1 => "The Music Man" , 2 => "South Pacific"),
+    
+                    "dramas" => array(0 => "Lawrence of Arabia" , 1 => "To Kill a Mockingbird " , 2 => "Casablanca"),
+                
+                    "mysteries" => array(0 => " The Maltese Falcon" , 1 => "Rear Window" , 2 => "North by Northwest"));
+      $Keys = array_keys($arr);
+      for($i=0 ; $i<count($arr); $i++){
+          echo strtoupper($Keys[$i])."<br/>";
+          foreach($arr[$Keys[$i]] as $key => $value){
+              echo "------> ".$key." = ".$value."<br/>";
+          }
+      }
+    
+    echo "<h2>Sort the array in decreasing order </h2>";
+    
+    krsort($arr);
+    $Keys = array_keys($arr);
+      for($i=0 ; $i<count($arr); $i++){
+          echo strtoupper($Keys[$i])."<br/>";
+          foreach($arr[$Keys[$i]] as $key => $value){
+              echo "------> ".$key." = ".$value."<br/>";
+          }
+      }
 
-//return an alert
-#palindrome("");
-
-//return an alert
-#palindrome(25);
-
-//return false
-#palindrome("Mario");
-
-//return true
-#palindrome("Mom");
-
-?>
+    ?>
 </body>
-
 </html>
